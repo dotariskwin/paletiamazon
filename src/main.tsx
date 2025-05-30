9bc7766c2ca3051cb54b1962494db6da5cfe7eec
dotariskwin <dotariskwin@gmail.com>
2025-05-30 15:03:30 +0300

    Adăugat Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 👉 Vercel Analytics
+import { inject } from '@vercel/analytics';
+inject();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
